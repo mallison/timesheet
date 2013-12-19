09c4d68bfd46e5815ad9053b12a2b25e2ed34846
Mark Allison <mark.allison@thebbgroup.org>
2013-12-19 09:57:34 +0000

    WIP more sensible way to show sub task times

--- timesheet/report.py
+++ timesheet/report.py
@@ -20,6 +20,27 @@ DATE_FORMAT = {
 REFLOG_RE = re.compile(r'^[0-9a-f]+ .*?\{([\d\-: ]+) \+.*?: (.*)$')
 
 
+class TaskLevelDict(defaultdict):
+    """TODO"""
+    def __init__(self):
+        super(TaskLevelDict, self).__init__(self.__class__)
+        self._total = timedelta(0)
+
+    def cumulative_total(self):
+        total = self._total
+        for sub_level in self.values():
+            total += sub_level.cumulative_total()
+        return total
+
+    @property
+    def total(self):
+        return self._total
+
+    @total.setter
+    def total(self, value):
+        self._total += value
+
+
 def group_slots_by_time_period(slots, resolution):
     """Group slots by year, month or day.
 
@@ -48,27 +69,47 @@ def show_groups(slots, resolutions=None, task_level=1, show_commits=False):
 
 
 def summarize(slots, task_level=1, show_commits=False):
-    totals = defaultdict(lambda: timedelta(0))
-    overall = timedelta(0)
+    totals = TaskLevelDict()
     slots = list(slots)
     for slot in slots:
         if slot['task'][0] not in AFK:
             duration = slot['end'] - slot['start']
-            totals[slot['task'][:task_level]] += duration
-            overall += duration
-    most = max([t[1] for t in totals.items()])
-    for task, total in sorted(totals.items(), key=itemgetter(1), reverse=True):
-        print "{:30s} {:15s} {}".format(
-            ': '.join(task)[:30],
-            man_days(total),
-            "#" * int(50 * total.total_seconds() / most.total_seconds())
-            )
-        if show_commits:
-            for slot in slots:
-                if slot['task'][:task_level] == task:
-                    print_commits(slot)
-            print
-    print "{:30s} {}".format("OVERALL", man_days(overall))
+            sub_level = totals
+            for i, part in enumerate(slot['task']):
+                sub_level = sub_level[part]
+            sub_level.total = duration
+    print man_days(totals.cumulative_total())
+    _summarize_level(totals, max_level=task_level - 1)
+    # most = max([t[1] for t in totals.items()])
+    # for task, total in sorted(totals.items(), key=itemgetter(1), reverse=True):
+    #     print "{:30s} {:15s} {}".format(
+    #         ': '.join(task)[:30],
+    #         man_days(total),
+    #         "#" * int(50 * total.total_seconds() / most.total_seconds())
+    #         )
+    #     if show_commits:
+    #         for slot in slots:
+    #             if slot['task'][:task_level] == task:
+    #                 print_commits(slot)
+    #         print
+    # print "{:30s} {}".format("OVERALL", man_days(overall))
+
+
+def _summarize_level(level_dict, level=0, max_level=0):
+    as_list = [(v.cumulative_total(), v.total, k)
+               for k, v in level_dict.items()]
+    as_list.sort(reverse=True)
+    for cumulative_duration, level_duration, name in as_list:
+        label = (' ' * level * 2) + name
+        print '%-40s  %s' % (label, man_days(cumulative_duration))
+        next_level = level + 1
+        if next_level <= max_level:
+            if level_duration and level_duration != cumulative_duration:
+                label = (' ' * (level + 1) * 2)
+                print '%-40s  %s' % (label, man_days(level_duration))
+            _summarize_level(level_dict[name],
+                             level=level + 1,
+                             max_level=max_level)
 
 
 _commits = None
