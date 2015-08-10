7e84a9eff77bbc98ba4fcd2174ae1c72aace9f3b
Mark Allison <mark@digital-impacts.org>
2015-08-10 21:18:15 +0100

    Too indented, refactor!

--- ts.py
+++ ts.py
@@ -18,21 +18,31 @@ def main():
         if m:
             time = utils.hhmm_to_minutes(m.group(1))
             if start:
-                parts = [p.strip() for p in task.split(':')]
-                if parts[0] not in ['afk', 'lunch']:
-                    sub_task = tally
-                    for part in ['main', day] + parts:
-                        if time < start:
-                            # assume wraps around midnight
-                            interval = 24 * 60 - start + time
-                        else:
-                            interval = time - start
-                        sub_task.setdefault(
-                            part, get_task())['total'] += interval
-                        sub_task = sub_task[part]['sub']
+                add_interval_to_tally(tally, day, task, start, time)
             start = time
             task = m.group(2) or 'misc'
     utils.print_task('main', tally['main'])
+
+
+def add_interval_to_tally(tally, day, task, start, end):
+    sub_tasks = [t.strip() for t in task.split(':')]
+    if sub_tasks[0] in ['afk', 'lunch']:
+        return
+    interval = get_interval(start, end)
+    sub_task_tally = tally
+    for sub_task in ['main', day] + sub_tasks:
+        sub_task_tally.setdefault(
+            sub_task, get_task())['total'] += interval
+        sub_task_tally = sub_task_tally[sub_task]['sub']
+
+
+def get_interval(start, end):
+    if end < start:
+        # assume wraps around midnight
+        interval = 24 * 60 - start + end
+    else:
+        interval = end - start
+    return interval
 
 
 def get_task():
